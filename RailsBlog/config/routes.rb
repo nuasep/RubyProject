aa0c87f60b2ba8b6098bb1b85c9a255c7e3fcd2f
nuasep <nuasep2604@gmail.com>
2018-05-20 19:57:32 +0700

    Initial commit

---
+++ RailsBlog/config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  get 'welcome/index'
+ 
+  root 'welcome#index'
+end
+
+Rails.application.routes.draw do
+  get 'welcome/index'
+ 
+  resources :articles
+ 
+  root 'welcome#index'
+end
